202e08c4fe264fa382a665ff867ef4f0fb408d9e
JB <me@me.com>
2024-02-13 14:39:00 -0600

    added csv file criteria

--- display_trends.py
+++ display_trends.py
@@ -1,11 +1,13 @@
-# I would like to see a wordcloud and a printout of all the most popular trends in New York City. I only care about
-# trends that have a tweet volumn of over 50,000. Use the file nyc_trends.py to extract the information and display 
-# it to the screen as well as in a wordcloud. Create a dictionary that will hold the tweet name as the key and the
+# I would like to see a wordcloud and a csv file of all the most popular trends in New York City. I only care about
+# trends that have a tweet volumn of over 50,000. Use the file nyc_trends.py to extract the information and write 
+# it to a file named trends.csv. Create a dictionary that will hold the tweet name as the key and the
 # tweet volume as the value. The dictionary must be named tweet_dict. You will need to install the library wordcloud
 # for the script to display the wordcloud. For reference, the wordclod should look exactly like the image - trends_original.png
+# and the output file should look like the file trends_original.csv
 
-# Create a VE for the wordcloud library and make sure the VE is not pushed to your GitHub repo.
+# Create a VE for the wordcloud library and make sure the VE IS NOT pushed to your GitHub repo.
 
+# Make sure your .gitignore file IS pushed to your GitHub repo.
 
 # nyc_trends is an object imported from the file nyc_trends.py
 from nyc_trends import nyc_trends
@@ -14,8 +16,15 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
 
+#create an output file
 
-#print out the type of object 'nyc_trends' is to see what object you are dealing with.
+
+#write the header for the output file
+
+
+
+#use the type function to print out the type of object 'nyc_trends' is to see what object you are dealing with.
+
 
 
 
@@ -25,7 +34,7 @@ tweet_dict = {}
 
 
 # iterate through the nyc_trends object to extract the tweet name and the tweet volume
-# and append it to the tweet_dict dictionary
+# and append it to the tweet_dict dictionary. Also write to the output file as you iterate.
 
 
 
@@ -39,9 +48,6 @@ wordcloud = WordCloud(colormap="prism", background_color="white").generate_from_
 plt.imshow(wordcloud)
 wordcloud.to_file('trends.png')
 plt.show()
-
-
-
 
 
 
